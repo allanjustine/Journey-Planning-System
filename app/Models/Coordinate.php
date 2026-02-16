fb1789d67e26df1bb3a8419c8f3f043abd08cef9
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2026-02-16 10:24:48 +0800

    add user!

--- app/Models/Coordinate.php
+++ app/Models/Coordinate.php
@@ -13,4 +13,9 @@ class Coordinate extends Model
     {
         return $this->hasMany(Activity::class);
     }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
